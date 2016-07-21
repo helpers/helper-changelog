66074b780850bcdd037dd2a2ae74e0dfe78a0f66
jonschlinkert <jon.schlinkert@sellside.com>
2016-07-21 05:37:21 -0400

    refactor

--- index.js
+++ index.js
@@ -1,23 +1,69 @@
 /*!
  * helper-changelog <https://github.com/jonschlinkert/helper-changelog>
  *
- * Copyright (c) 2015, Jon Schlinkert.
+ * Copyright (c) 2015-2016, Jon Schlinkert.
  * Licensed under the MIT License.
  */
 
 'use strict';
 
+var fs = require('fs');
+var path = require('path');
 var stringify = require('stringify-changelog');
 var merge = require('mixin-deep');
 
-module.exports = function changelog(app) {
-  var val = app && app.option && app.option('changelog');
-  return function (data, options) {
-    data = data || val || null;
+module.exports = function(options) {
+  options = options || {};
 
-    if (!data) {
-      throw new Error('helper-changelog cannot find data or a file to read.');
+  return function(changelog, locals) {
+    var opts = merge({}, options, locals);
+    if (this && this.context) {
+      opts = merge({}, this.options, this.context, opts);
     }
-    return stringify(data, options);
+
+    changelog = changelog || opts.changelog;
+    if (typeof changelog === 'string') {
+      var ext = path.extname(changelog);
+      var cwd = opts.cwd || process.cwd();
+      var fp = path.resolve(cwd, changelog);
+      var str = fs.readFileSync(fp, 'utf8');
+
+      if (ext && !/\.(json|ya?ml)$/.test(ext)) {
+        var repo = getRepo(opts);
+        if (repo) str += '\n' + createLinks(str, repo);
+        return str;
+      }
+    }
+    return stringify(changelog, opts);
   };
 };
+
+function getRepo(ctx) {
+  var repo = ctx.repo;
+  if (repo) return repo;
+  if (ctx.name && (ctx.owner || ctx.username)) {
+    return (ctx.owner || ctx.username) + '/' + ctx.name;
+  }
+}
+
+function createLinks(str, repo) {
+  var re = /^#+ (\[[^\]]+\])/gm;
+  var range = (str.match(re) || []).map(function(str) {
+    return str.replace(/^#+ \[|\]$/g, '');
+  });
+
+  var len = range.length;
+  var idx = -1;
+  var res = [];
+  while (++idx < len) {
+    var curr = range[idx];
+    var prev = range[idx + 1];
+    if (!prev) break;
+    res.push(createLink(curr, prev, repo));
+  }
+  return res.join('\n');
+}
+
+function createLink(curr, prev, repo) {
+  return `[${curr}]: https://github.com/${repo}/compare/${prev}...${curr}`;
+}
--- test.js
+++ test.js
@@ -7,104 +7,211 @@
 
 'use strict';
 
-/* deps:mocha */
+require('mocha');
 var assert = require('assert');
-var should = require('should');
 var moment = require('moment');
-var Template = require('template');
+var App = require('templates');
 var changelog = require('./');
-var template, helper;
+var app, helper;
 
-describe('changelog', function () {
-  beforeEach(function () {
-    template = new Template();
-    helper = changelog(template);
-    template.helper('changelog', helper);
+describe('changelog', function() {
+  beforeEach(function() {
+    app = new App();
+    app.create('pages');
+    app.engine('md', require('engine-base'));
+    app.helper('changelog', changelog());
   })
 
-  it('should generate a changelog from a yaml file:', function (done) {
-    template.page('foo.md', {content: '<%= changelog("fixtures/a.yml") %>'});
-    template.render('foo.md', function (err, content) {
-      if (err) console.log(err);
+  it('should generate a changelog from a yaml file:', function(cb) {
+    app.page('foo.md', {content: '<%= changelog("fixtures/a.yml") %>'});
+    app.render('foo.md', function(err, res) {
+      if (err) return cb(err);
 
-      content.should.equal([
-        '**DATE**       **VERSION**   **CHANGES**                  ',
-        '* 2016-12-26   v0.1.0        Got stuck in another chimney.',
+      assert.equal(res.content, [
+        '## [v0.2.0] - 2016-12-26',
+        '',
+        '**changes**',
+        '',
+        '- Got stuck in another chimney.',
+        '',
+        '## [v0.1.0] - 2015-12-26',
+        '',
+        '**changes**',
+        '',
+        '- Got stuck in a chimney last night.',
+        ''
       ].join('\n'));
-      done();
+      cb();
     });
   });
 
-  it('should generate a changelog from an object passed to the helper:', function (done) {
-    var str = '<%= changelog({ "v0.1.0": { date: "2016-12-26", changes: [ "Got stuck in another chimney." ] } }) %>';
-
-    template.page('bar.md', {content: str});
-    template.render('bar.md', function (err, content) {
-      if (err) console.log(err);
-
-      content.should.equal([
-        '**DATE**       **VERSION**   **CHANGES**                  ',
-        '* 2016-12-26   v0.1.0        Got stuck in another chimney.',
-      ].join('\n'));
-      done();
+  it('should generate a changelog from a markdown file:', function(cb) {
+    app.page('foo.md', {content: '<%= changelog("fixtures/changelog.md") %>'});
+    app.render('foo.md', function(err, res) {
+      if (err) return cb(err);
+      assert(/### key/.test(res.content));
+      cb();
     });
   });
 
-  it('should generate a changelog from an array passed to the helper:', function (done) {
-    var str = '<%= changelog([{date: "2016-12-26", version: "v0.1.0", changes: [ "Got stuck in another chimney." ]}]) %>';
-
-    template.page('bar.md', {content: str});
-    template.render('bar.md', function (err, content) {
-      if (err) console.log(err);
-
-      content.should.equal([
-        '**DATE**       **VERSION**   **CHANGES**                  ',
-        '* 2016-12-26   v0.1.0        Got stuck in another chimney.',
-      ].join('\n'));
-      done();
+  it('should add reflinks for headings when "owner/name" is on the context', function(cb) {
+    app.page('foo.md', {content: '<%= changelog("fixtures/changelog.md") %>'});
+    app.data({name: 'assemble-core', owner: 'assemble'});
+    app.render('foo.md', function(err, res) {
+      if (err) return cb(err);
+      assert(/### key/.test(res.content));
+      assert(res.content.indexOf('0.25.0...0.26.0') !== -1);
+      cb();
     });
   });
 
-  it('should generate a changelog from an array passed on the context:', function (done) {
+  it('should use "owner/name" passed on helper locals', function(cb) {
+    app.page('foo.md', {content: '<%= changelog("fixtures/changelog.md", {name: "assemble-core", owner: "assemble"}) %>'});
+    app.render('foo.md', function(err, res) {
+      if (err) return cb(err);
+      assert(/### key/.test(res.content));
+      assert(res.content.indexOf('0.25.0...0.26.0') !== -1);
+      cb();
+    });
+  });
+
+  it('should use "owner/name" passed on render locals', function(cb) {
+    app.page('foo.md', {content: '<%= changelog("fixtures/changelog.md") %>'});
+    app.render('foo.md', {name: 'assemble-core', owner: 'assemble'}, function(err, res) {
+      if (err) return cb(err);
+      assert(/### key/.test(res.content));
+      assert(res.content.indexOf('0.25.0...0.26.0') !== -1);
+      cb();
+    });
+  });
+
+  it('should add reflinks for headings when "repo" is on the context', function(cb) {
+    app.page('foo.md', {content: '<%= changelog("fixtures/changelog.md") %>'});
+    app.data({repo: 'assemble/assemble-core'});
+    app.render('foo.md', function(err, res) {
+      if (err) return cb(err);
+      assert(/### key/.test(res.content));
+      assert(res.content.indexOf('0.25.0...0.26.0') !== -1);
+      cb();
+    });
+  });
+
+  it('should use "repo" passed on helper locals', function(cb) {
+    app.page('foo.md', {content: '<%= changelog("fixtures/changelog.md", {repo: "assemble/assemble-core"}) %>'});
+    app.render('foo.md', function(err, res) {
+      if (err) return cb(err);
+      assert(/### key/.test(res.content));
+      assert(res.content.indexOf('0.25.0...0.26.0') !== -1);
+      cb();
+    });
+  });
+
+  it('should use "repo" passed on render locals', function(cb) {
+    app.page('foo.md', {content: '<%= changelog("fixtures/changelog.md") %>'});
+    app.render('foo.md', {repo: 'assemble/assemble-core'}, function(err, res) {
+      if (err) return cb(err);
+      assert(/### key/.test(res.content));
+      assert(res.content.indexOf('0.25.0...0.26.0') !== -1);
+      cb();
+    });
+  });
+
+  it('should not fail when headings do not match', function(cb) {
+    app.page('foo.md', {content: '<%= changelog("fixtures/foo.md") %>'});
+    app.data({name: 'assemble-core', owner: 'assemble'});
+    app.render('foo.md', function(err, res) {
+      if (err) return cb(err);
+      assert(/### key/.test(res.content));
+      cb();
+    });
+  });
+
+  it('should generate a changelog from an object passed to the helper:', function(cb) {
+    var str = '<%= changelog({ "v0.2.0": { date: "2016-12-26", changes: [ "Got stuck in another chimney." ] } }) %>';
+
+    app.page('bar.md', {content: str});
+    app.render('bar.md', function(err, res) {
+      if (err) return cb(err);
+
+      assert.equal(res.content, [
+        '## [v0.2.0] - 2016-12-26',
+        '',
+        '**changes**',
+        '',
+        '- Got stuck in another chimney.',
+        '',
+      ].join('\n'));
+      cb();
+    });
+  });
+
+  it('should generate a changelog from an array passed to the helper:', function(cb) {
+    var str = '<%= changelog([{date: "2016-12-26", version: "v0.2.0", changes: [ "Got stuck in another chimney." ]}]) %>';
+
+    app.page('bar.md', {content: str});
+    app.render('bar.md', function(err, res) {
+      if (err) return cb(err);
+
+      assert.equal(res.content, [
+        '## [v0.2.0] - 2016-12-26',
+        '',
+        '**changes**',
+        '',
+        '- Got stuck in another chimney.',
+        '',
+      ].join('\n'));
+      cb();
+    });
+  });
+
+  it('should generate a changelog from an array passed on the context:', function(cb) {
     var data = {
       changes: [
-        {date: '2016-12-26', version: 'v0.1.0', changes: [ 'Got stuck in another chimney.' ]
+        {date: '2016-12-26', version: 'v0.2.0', changes: [ 'Got stuck in another chimney.' ]
       }]
     };
 
-    template.page('bar.md', {content: '<%= changelog(changes) %>'});
-    template.render('bar.md', data, function (err, content) {
-      if (err) console.log(err);
+    app.page('bar.md', {content: '<%= changelog(changes) %>'});
+    app.render('bar.md', data, function(err, res) {
+      if (err) return cb(err);
 
-      content.should.equal([
-        '**DATE**       **VERSION**   **CHANGES**                  ',
-        '* 2016-12-26   v0.1.0        Got stuck in another chimney.',
+      assert.equal(res.content, [
+        '## [v0.2.0] - 2016-12-26',
+        '',
+        '**changes**',
+        '',
+        '- Got stuck in another chimney.',
+        '',
       ].join('\n'));
-      done();
+      cb();
     });
   });
 
-  it('should allow a custom date function:', function (done) {
+  it('should allow a custom date function:', function(cb) {
     var data = {
       changes: [
-        {date: '2016-12-26', version: 'v0.1.0', changes: [ 'Got stuck in another chimney.' ]
+        {date: '2016-12-26', version: 'v0.2.0', changes: [ 'Got stuck in another chimney.' ]
       }],
       opts: {
-        dateFn: function (date) {
+        dateFn: function(date) {
           return ' * ' + moment(date).format('YYYY-MM-DD');
         }
       }
     };
 
-    template.page('bar.md', {content: '<%= changelog(changes, opts) %>'});
-    template.render('bar.md', data, function (err, content) {
-      if (err) console.log(err);
+    app.page('bar.md', {content: '<%= changelog(changes, opts) %>'});
+    app.render('bar.md', data, function(err, res) {
+      if (err) return cb(err);
 
-      content.should.equal([
-        '**DATE**       **VERSION**   **CHANGES**                  ',
-        '* 2016-12-26   v0.1.0        Got stuck in another chimney.',
+      assert.equal(res.content, [
+        '## [v0.2.0] - 2016-12-26',
+        '',
+        '**changes**',
+        '',
+        '- Got stuck in another chimney.',
+        '',
       ].join('\n'));
-      done();
+      cb();
     });
   });
 });
